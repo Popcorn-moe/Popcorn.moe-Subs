761c83ebfdb9f946053b4abdb1891e29519c5203
SkyBeastMC <skybeastmc@gmail.com>
2018-04-22 20:56:21 +0200

    Added POST

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import express from 'express'
 import logger from 'morgan'
 import Redis from 'ioredis'
 import fs from 'fs'
+import bodyParser from 'body-parser'
+import srtToVtt from 'srt-to-vtt'
+import assToVtt from 'ass-to-vtt'
+import str from 'string-to-stream'
+import toString from 'stream-to-string'
 
 const app = express()
 const redis = new Redis()
 
 app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/:anime/:season/:episode/:lang', async (req, res) => {
 	const { anime, season, episode, lang } = req.params
@@ -22,21 +28,70 @@ app.get('/:anime/:season/:episode/:lang', async (req, res) => {
 
 	res.set('info', JSON.stringify(info))
 	res.type('text')
-	res.send(toSend)
+	res.end(toSend)
+})
+
+const conversion = {
+	vtt: content => content,
+	ass: content => toString(str(content).pipe(assToVtt())),
+	srt: content => toString(str(content).pipe(srtToVtt())) //is there a better way?
+}
+
+app.post('/:anime/:season/:episode/:lang', async (req, res) => {
+	const { anime, season, episode, lang } = req.params
+
+	const { info, content, type } = req.body
+	const conv = conversion[type]
+
+	if (!content || !type || !conv) {
+		res.sendStatus(400)
+		return
+	}
+
+	const converted = await conv(content)
+
+	const i = info || extractInfos(converted) || {}
+
+	const withInfo = info ? insertInfo(converted, i) : converted
+
+	redis.set(`${anime}/${season}/${episode}/${lang}`, withInfo)
+
+	res.set('info', JSON.stringify(info))
+	res.type('text')
+	res.end(withInfo)
 })
 
 const header = fs.readFileSync('header.txt').toString()
 
 function insertHeader(content, info = {}) {
-	const subber = (info.subber || 'Unknown') + (info.subber_link ? ' <' + info.subber_link + '>' : '')
-	const insertion = header.replace('$INFO', info).replace('$SUBBER', subber)
+	const subber =
+		(info.subber || 'Unknown') +
+		(info.subber_link ? ' <' + info.subber_link + '>' : '')
 
-	const index = content.indexOf('\n') + 1 //Insert after first \n\n
-	return content.substring(0, index) + '\n' + insertion + content.substring(index)
+	return insertNote(content, header.replace('$INFO', info).replace('$SUBBER', subber))
+}
+
+function insertInfo(content, info = {}) {
+	return insertNote(content, `NOTE ${JSON.stringify({ info })}\n`)
+}
+
+function insertNote(content, insertion) {
+	const index = content.indexOf('\n') + 1 //Insert after first \n
+	return (
+		content.substring(0, index) + '\n' + insertion + content.substring(index)
+	)
 }
 
 function extractInfos(subs) {
-	return JSON.parse(/{"info": {.+}}/.exec(subs)[0]).info;
+	const res = /{"info": ?{.+}}/.exec(subs)
+	if (!res) return
+	let o
+	try {
+		o = JSON.parse(res[0])
+	} catch (e) {
+		return
+	}
+	return o && o.info
 }
 
 app.listen(3333, () => console.log('Listening on port 3333'))
